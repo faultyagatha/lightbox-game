07cea0d46da754935e1ab65e5f957fc9b58d694c
faultyagatha <valeria.barvinska@gmail.com>
2020-11-05 21:28:57 +0100

    add class-based game

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Board from './components/Board';
+
+const App = () => {
+  const defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceOfLight: 0.25
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Board nRows={defaultProps.nrows} nCols={defaultProps.ncols} chanceOfLight={defaultProps.chanceOfLight} />
     </div>
   );
 }
--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React from 'react';
 
-const Cell = () => {
-  return (
-    <div>
-      Cell
-    </div>
-  )
+import './index.css';
+
+// type CellProps = (
+//   {
+//     isLit,
+//   }: {
+//     isLit: boolean;
+//     // flipCellsAround(): void
+//   }
+// ) => JSX.Element;
+
+// const Cell: CellProps = ({ isLit, }) => {
+
+//   const handleClick = () => {
+//     // props.flipCellsAround!();
+//     console.log('clicked');
+//   };
+
+//   let classes = "cell" + (isLit ? " cell-lit" : "");
+
+//   return (
+//     <td className="cell-lit" onClick={handleClick} />
+//   )
+// }
+
+// export default Cell;
+
+/** A single cell on the board.
+ *
+ * This has no state --- just two props:
+ *
+ * - flipCellsAroundMe: a function rec'd from the board which flips this
+ *      cell and the cells around of it
+ *
+ * - isLit: boolean, is this cell lit?
+ *
+ * This handles clicks --- by calling flipCellsAroundMe
+ *
+ **/
+
+/** interface describing properies of a stateless component */
+interface ICellProps {
+  isLit: boolean;
+  flipCellsAround(): void;
+};
+
+
+class Cell extends React.Component<ICellProps> {
+  constructor(props: any) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e: any) {
+    // call up to the board to flip cells around this cell
+    this.props.flipCellsAround();
+  }
+
+  render() {
+    let classes = "cell" + (this.props.isLit ? " cell-lit" : "");
+
+    return (
+      <td className={classes} onClick={this.handleClick} />
+    )
+  }
 }
+
 
 export default Cell;
